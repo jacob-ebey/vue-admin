8bf0c007899befad3cfa1e74319ccafd89865e93
Jacob Ebey <jacob.ebey@live.com>
2017-11-18 23:04:07 -0800

    - Updated again

--- client/store/actions.js
+++ client/store/actions.js
@@ -197,12 +197,11 @@ export const setToken = ({ commit }, token) => {
 
 // TODO: Find a better place for this
 const isDev = process.env.NODE_ENV !== 'production'
+// eslint-disable-next-line no-undef
+const loc = isDev ? 'localhost:4040' : location.host
 
 // eslint-disable-next-line no-undef
-const loc = isDev ? 'localhost:4040' : location.origin.replace(/^http[s]?/, location.protocol !== 'https:' ? 'ws' : 'wss')
-
-// eslint-disable-next-line no-undef
-var ws = new WebSocket('ws://' + loc)
+var ws = new WebSocket(location.protocol !== 'https:' ? 'ws://' : 'wss://' + loc)
 let isWebsocketOpen = false
 const eventQueue = []
 ws.onopen = function () {
